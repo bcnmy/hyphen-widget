c78cba43fcfdca009c3055fa7181deb942908036
Asheer Rizvi <asheer.vil@gmail.com>
2022-05-10 11:00:02 +0530

    chore: changed deposit tag for widget.

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,7 +1,7 @@
-export const NATIVE_ADDRESS = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
+export const NATIVE_ADDRESS = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
 export const DEFAULT_FIXED_DECIMAL_POINT = 5;
 export const LP_FEE_FRACTION = 0.1;
-export const DEPOSIT_TAG = 'HYPHEN_UI';
+export const DEPOSIT_TAG = "HYPHEN_WIDGET";
 export const BASE_DIVISOR = 100000000;
 // export const MANUAL_EXIT_RETRIES = 12;
 
