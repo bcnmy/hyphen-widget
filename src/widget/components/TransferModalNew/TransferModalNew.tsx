b78a262986ba84bd9230fa9c7671729fbb0180cc
Asheer Rizvi <asheer.vil@gmail.com>
2022-08-31 13:14:37 +0530

    feat: pre deposit state for transfer modal

--- src/widget/components/TransferModalNew/TransferModalNew.tsx
+++ src/widget/components/TransferModalNew/TransferModalNew.tsx
@@ -1,6 +1,12 @@
 import { Dialog, Transition } from '@headlessui/react';
 import Modal from 'components/Modal';
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
+import loadingSpinner from 'assets/images/loading-spinner.svg';
+import PrimaryButton from 'components/Buttons/PrimaryButton';
+import { HiExclamation } from 'react-icons/hi';
+import { useTransaction } from 'context/Transaction';
+import { Status } from 'hooks/useLoading';
+import { useToken } from 'context/Token';
 
 export interface ITransferModalProps {
   isOpen: boolean;
@@ -8,6 +14,73 @@ export interface ITransferModalProps {
 }
 
 function TransferModalNew({ isOpen, closeModal }: ITransferModalProps) {
+  const {
+    executePreDepositCheck,
+    executePreDepositCheckError,
+    executePreDepositCheckStatus,
+    executeDeposit,
+    executeDepositStatus,
+    executeDepositValue,
+    executeDepositError,
+    checkReceival,
+    receiver: { receiverAddress },
+  } = useTransaction()!;
+  const { refreshSelectedTokenBalance } = useToken()!;
+
+  const [exitHash, setExitHash] = useState('');
+  const [deposited, setDeposited] = useState(false);
+
+  // useEffect(() => {
+  //   console.log('useEffect 1 fired!');
+  //   executePreDepositCheck();
+  //   if (executePreDepositCheckStatus === Status.SUCCESS) {
+  //     executeDeposit(receiverAddress);
+  //   }
+  // }, [
+  //   executeDeposit,
+  //   executePreDepositCheck,
+  //   executePreDepositCheckStatus,
+  //   receiverAddress,
+  // ]);
+
+  // useEffect(() => {
+  //   console.log('useEffect 2 fired!');
+  //   if (executeDepositStatus === Status.SUCCESS) {
+  //     (async () => {
+  //       await executeDepositValue.wait(1);
+  //       setDeposited(true);
+  //     })();
+  //   }
+  // }, [executeDepositStatus, executeDepositValue]);
+
+  // useEffect(() => {
+  //   console.log('useEffect 3 fired!');
+  //   if (deposited) {
+  //     let tries = 0;
+  //     let keepChecking = setInterval(async () => {
+  //       try {
+  //         tries++;
+  //         let hash = await checkReceival();
+  //         if (hash) {
+  //           clearInterval(keepChecking);
+  //           // hideManualExit();
+  //           refreshSelectedTokenBalance();
+  //           setExitHash(hash);
+  //         }
+  //         // else if (tries > MANUAL_EXIT_RETRIES) {
+  //         //   showManualExit();
+  //         // }
+  //         else if (tries > 300) {
+  //           clearInterval(keepChecking);
+  //           throw new Error('exhauseted max retries');
+  //         }
+  //       } catch (e) {
+  //         console.error(e);
+  //       }
+  //     }, 5000);
+  //   }
+  // }, [checkReceival, deposited, refreshSelectedTokenBalance]);
+
   return (
     <Transition appear show={isOpen} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={closeModal}>
@@ -34,8 +107,30 @@ function TransferModalNew({ isOpen, closeModal }: ITransferModalProps) {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <div className="h-[464px] w-[464px] rounded-[25px] bg-white p-12.5">
-                Hello World!
+              <div className="flex h-[446px] w-[464px] flex-col rounded-[25px] bg-white p-12.5">
+                <Dialog.Title
+                  as="h3"
+                  className="mb-10 text-left text-base font-semibold text-hyphen-purple"
+                >
+                  Transfer Activity
+                </Dialog.Title>
+
+                <img
+                  src={loadingSpinner}
+                  alt="Loading..."
+                  className="mx-auto mb-auto animate-spin"
+                />
+
+                <PrimaryButton className="mb-3 bg-hyphen-gray-100 text-base font-semibold text-hyphen-gray-400">
+                  Checking Available Liquidity...
+                </PrimaryButton>
+
+                <article className="flex items-center justify-center rounded-[10px] bg-hyphen-warning bg-opacity-25 p-2 text-hyphen-warning">
+                  <HiExclamation className="mr-2 h-3 w-3" />
+                  <p className="text-xxs font-bold uppercase">
+                    Please do not refresh or change network.
+                  </p>
+                </article>
               </div>
             </Transition.Child>
           </div>
